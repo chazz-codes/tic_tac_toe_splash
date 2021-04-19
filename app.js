f0d15ef8da17ed60ac2163c76eb1d57544934d90
chazzsplash <chazzsplash@gmail.com>
2021-04-19 11:12:53 -0400

    tic tac toe board populates but needs improved logic for restart and win

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const selectBox = document.querySelector(".select-box"),
-    selectXbtn = document.querySelector(".playerX"),
-    selectOBtn = document.querySelector(".playerO"),
-    board = document.querySelector(".board"),
-    players = document.querySelector(".players"),
-    allBox = document.querySelectorAll("section div"),
-    results = document.querySelector(".results"),
-    
+document.addEventListener('DOMContentLoaded', () =>{
+    const squares = document.querySelectorAll('.grid div')
+    const playerDisplay = document.querySelector('#player')
 
-window.onload = () =>{
-    selectBox.classList.add("hide");
-}
+    let currentPlayer = "playerX"
+    
+    squares.forEach(square => {
+        square.addEventListener('click', clickOutcome)
+    })
+
+    function clickOutcome(e) { 
+        const squareArray = Array.from(squares)
+        const index = squareArray.indexOf(e.target )
+        console.log(index)
+        playerDisplay.innerHTML = currentPlayer
+
+        if (currentPlayer === "playerX"){
+            squares[index].classList.add('playerX')
+            currentPlayer = "playerO"
+        } else {
+            squares[index].classList.add('playerO')
+            currentPlayer = "playerX"
+        }
+    }
+})
