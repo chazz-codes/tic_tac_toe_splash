e52fe6e3f462d29ef1a5f9746cedb9d06b20c3f4
chazzsplash <chazzsplash@gmail.com>
2021-04-19 09:50:01 -0400

    started over, last code not functioning

--- app.js
+++ app.js
@@ -1,79 +1,12 @@
-const X_CLASS = 'x'
-const CIRCLE_CLASS = 'circle'
-const WINNING_COMBINATIONS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8], 
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-]
+const selectBox = document.querySelector(".select-box"),
+    selectXbtn = document.querySelector(".playerX"),
+    selectOBtn = document.querySelector(".playerO"),
+    board = document.querySelector(".board"),
+    players = document.querySelector(".players"),
+    allBox = document.querySelectorAll("section div"),
+    results = document.querySelector(".results"),
+    
 
-
-const cellElements = document.getElementsByClassName("gamecell");
-const boardElements = document.getElementsByClassName("gameboard");
-const winningMessageTextElement = document.getElementById("popup");
-
-
-let circleTurn
-
-startGame()
-
-function startGame(){
-
-cellElements.forEach(cell =>{
-    gamecell.addEventListener('click', handleClick, {once: true})
-})
-
-function handleClick(e) {
-const cell = e.target
-const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
-placeMark(gamecell, currentClass)
-if (checkWin(currentClass)) {
-    endGame(false)
+window.onload = () =>{
+    selectBox.classList.add("hide");
 }
-
-
-
-
-swapTurns()
-setBoardHoverClass()
-}
-
-function endGame(draw){
-    if (draw) {
-
-    } else {
-        winningMessageTextElement
-    }
-}
-
-function placeMark(gamecell, currentClass){
-    if (checkWin(currentClass)){
- gamecell.classList.add(currentClass)
-}}
-
-function swapTurns(){
-    circleTurn = !circleTurn
-}
-
-function setBoardHoverClass() {
-gameboard.classList.remove(X_CLASS)
-gameboard.classList.remove(CIRCLE_CLASS)
-if (circleTurn) {
-    gameboard.classList.add(CIRCLE_CLASS)
-} else {
-    gameboard.classList.add(X_CLASS)
-    }
-}
-
-function checkWin(currentClass) {
-    return WINNING_COMBINATIONS.some(combination => {
-        return combination.every(index => {
-            return cellElements[index].classList.contains(currentClass)
-        })
-    })
-}}
-
