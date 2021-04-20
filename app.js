c6609ac84d5edc2c1a3ec255641e1fb89886b340
chazzsplash <chazzsplash@gmail.com>
2021-04-19 23:22:58 -0400

    trucking along, struggling with this algorithm

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // establish global contants
 document.addEventListener('DOMContentLoaded', () =>{
-    const squares = document.querySelectorAll('.grid div')
+    const squares = document.querySelectorAll('.grid div') 
     const playerDisplay = document.querySelector('#player')
     const WINNING_COMBINATIONS = [
         [0, 1, 2],
@@ -12,7 +12,11 @@ document.addEventListener('DOMContentLoaded', () =>{
         [0, 4, 8],
         [2, 4, 6]
     ]
-    const squareArray = Array.from(squares)
+    let gameState = ["", "", "", "", "", "", "", "", ""];
+    const winningMessageElement = document.getElementById('winning-msg')
+    const winningMessageTextElement = document.getElementsByClassName('winning-msg-txt')
+    const squareArray = Array.from(squares) // array not filled with values, empty cells
+    console.log(squareArray) 
     var currentPlayer
  
     startGame()
@@ -31,12 +35,17 @@ document.addEventListener('DOMContentLoaded', () =>{
         }
     
 
+
+
     // logic to populate square with an icon  --- Works
     function clickOutcome(e) { 
         playerDisplay.innerHTML = currentPlayer === "playerX" ? "O's Turn" : "X's Turn"  //displays current player at player display area
         const cell = e.target
         const index = squareArray.indexOf(cell) //provides numerical output of click location 
+        
         console.log(index)
+        console.log(e.target.classList)
+        cell.classList.add(currentPlayer)
                 
     //logic to determine which icon gets added to clicked square
             if (currentPlayer === "playerX"){
@@ -47,20 +56,38 @@ document.addEventListener('DOMContentLoaded', () =>{
                 squares[index].classList.add('playerO')
                 currentPlayer = "playerX"
             } //closing to else statement
-        
-            if (checkWin(currentPlayer)){
-                
-            }
 
-        
 
-    // check for winning outcome function --- run this every time user clicks
-        function checkWin(currentPlayer) {
-            return WINNING_COMBINATIONS.some(combination => {
-                // if some array in combinations has every index filled with currentPlayer, function is true
-                return combination.every(index => {
-                return squareArray[index].classList.contains(currentPlayer)
-                })})} //closing to check win function 
+            function handleResultValidation() {
+                let roundWon = false;
+                for (let i = 0; i <= 7; i++) {
+                    const winCondition = WINNING_COMBINATIONS[i];
+                    let a = gameState[winCondition[0]];
+                    let b = gameState[winCondition[1]];
+                    let c = gameState[winCondition[2]];
+                    if (a === '' || b === '' || c === '') {
+                        continue;
+                    }
+                    if (a === b && b === c) {
+                        roundWon = true;
+                        break
+                    }
+                }
+            
+                if (roundWon) {
+                    statusDisplay.innerHTML = winningMessage();
+                    gameActive = false;
+                    return;
+                }
+            
+                let roundDraw = !gameState.includes("");
+                if (roundDraw) {
+                    statusDisplay.innerHTML = drawMessage();
+                    gameActive = false;
+                    return;
+                }
+        
+ 
                 
     } // closing to click outcome function 
 
